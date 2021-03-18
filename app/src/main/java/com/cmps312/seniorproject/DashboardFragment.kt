53d2b44c2cd74a311cc88921baba7ed30ec314ef
Fahad-fa1408325 <point2void@gmail.com>
2021-03-18 09:55:15 +0300

    add and remove previous alarm

--- app/src/main/java/com/cmps312/seniorproject/DashboardFragment.kt
+++ app/src/main/java/com/cmps312/seniorproject/DashboardFragment.kt
@@ -1,20 +1,35 @@
 package com.cmps312.seniorproject
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.cmps312.seniorproject.model.alarm.AlarmReceiver
+import com.cmps312.seniorproject.ui.viewmodel.PillViewModel
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_dashboard.*
+import java.util.*
 
 
 class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
+    private val pillViewModel: PillViewModel by activityViewModels()
 
+    private var alarmMgr: AlarmManager? = null
+    private lateinit var alarmIntent: PendingIntent
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         val auth = FirebaseAuth.getInstance()
+
+        removePreviousAlarms()
+        addPreviousAlarms()
 
         logOutCardView.setOnClickListener {
             activity?.onBackPressed()
@@ -42,6 +57,97 @@ class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
             findNavController().navigate(R.id.action_dashboardFragment_to_dispenseOnDemandFragment)
         }
 
+    }
+
+    private fun removePreviousAlarms() {
+
+        if (!pillViewModel.loggedInFlag) {
+
+            pillViewModel.getAllPills()
+            pillViewModel.tempPills.observe(viewLifecycleOwner) { pill ->
+                pill.forEach {
+                    var time = it.time.split(":")
+                    var hours = time[0].toInt()
+                    var minutes = time[1].toInt()
+
+                    alarmMgr = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                    alarmIntent = Intent(context, AlarmReceiver::class.java).let { intent ->
+                        intent.putExtra("message", it.name)
+                        PendingIntent.getBroadcast(
+                            context,
+                            it.requestKey,
+                            intent,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                        )
+                    }
+
+                    val cal: Calendar = Calendar.getInstance().apply {
+                        timeInMillis = System.currentTimeMillis()
+                        set(Calendar.HOUR_OF_DAY, hours)
+                        set(Calendar.MINUTE, minutes)
+                        set(Calendar.SECOND, 0)
+                    }
+
+                    alarmMgr?.set(
+                        AlarmManager.RTC_WAKEUP, cal.timeInMillis,
+                        alarmIntent
+                    )
+
+                    alarmMgr?.cancel(alarmIntent)
+
+                }
+            }
+        }
+    }
+
+    private fun addPreviousAlarms() {
+
+        if (!pillViewModel.loggedInFlag) {
+            pillViewModel.getPills(pillViewModel.currentUser.uid)
+
+            pillViewModel.pills.observe(viewLifecycleOwner) { pill ->
+                pill.forEach {
+                    var time = it.time.split(":")
+                    var hours = time[0].toInt()
+                    var minutes = time[1].toInt()
+
+
+                    alarmMgr = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                    alarmIntent = Intent(context, AlarmReceiver::class.java).let { intent ->
+                        intent.putExtra("message", it.name)
+                        PendingIntent.getBroadcast(
+                            context,
+                            it.requestKey,
+                            intent,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                        )
+                    }
+
+                    val cal: Calendar = Calendar.getInstance().apply {
+                        timeInMillis = System.currentTimeMillis()
+                        set(Calendar.HOUR_OF_DAY, hours)
+                        set(Calendar.MINUTE, minutes)
+                        set(Calendar.SECOND, 0)
+                    }
+
+                    alarmMgr?.set(
+                        AlarmManager.RTC_WAKEUP, cal.timeInMillis,
+                        alarmIntent
+                    )
+
+                    alarmMgr?.setRepeating(
+                        AlarmManager.RTC_WAKEUP,
+                        cal.timeInMillis,
+                        3600000 * it.dosage.toString().toLong(),
+                        alarmIntent
+                    )
+
+                }
+            }
+
+            pillViewModel.loggedInFlag = true
+
+        }
     }
 
 }
